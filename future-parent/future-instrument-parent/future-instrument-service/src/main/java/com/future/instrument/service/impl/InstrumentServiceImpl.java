7a8432a8e8c19c99af67c15f12280fec50dab2ce
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-02-14 12:54:40 +0800

    合约服务

---
+++ future-parent/future-instrument-parent/future-instrument-service/src/main/java/com/future/instrument/service/impl/InstrumentServiceImpl.java
@@ -0,0 +1,40 @@
+package com.future.instrument.service.impl;
+
+import java.util.List;
+
+import com.future.instrument.api.service.InstrumentService;
+import com.future.instrument.api.vo.InstrumentVO;
+
+public class InstrumentServiceImpl implements InstrumentService{
+
+    @Override
+    public InstrumentVO queryInstrument(String instrumentID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public InstrumentVO queryInstrument(String instrumentID, String tradingDate) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> queryInstrumentName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> queryInstrumentName(String tradingDate) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void saveInstrument(InstrumentVO instrumentVO) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
