779f72408e2550b29d2cabc13e1947c5e1a9420c
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-02-23 16:34:26 +0800

    创建交易工程

---
+++ future-parent/future-trade-parent/future-trade-api/src/main/java/com/future/trade/api/service/TradeService.java
@@ -0,0 +1,16 @@
+package com.future.trade.api.service;
+
+/**
+ * 交易接口
+ * @author caojia
+ *
+ */
+public interface TradeService {
+    
+    /**
+     * 下单接口
+     * @return
+     */
+    public int reqOrderInsert();
+
+}
---
+++ future-parent/future-trade-parent/future-trade-api/src/main/java/com/future/trade/api/vo/ReqOrderInsertVO.java
@@ -0,0 +1,107 @@
+package com.future.trade.api.vo;
+
+import java.io.Serializable;
+
+/**
+ * 下单参数
+ * @author caojia
+ *
+ */
+public class ReqOrderInsertVO implements Serializable{
+    
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -7390751012439707057L;
+
+    
+  ///请求编号
+    TThostFtdcRequestIDType RequestID;
+    
+    /**
+     * 经纪公司代码
+     */
+    private String brokerID;
+    
+    /**
+     * 投资者代码
+     */
+    private String investorID;
+    
+    /**
+     * 合约代码
+     */
+    private String instrumentID;
+    
+    /**
+     * 报单引用
+     */
+    private String orderRef;
+    
+    /**
+     * 用户代码
+     */
+    private String userID;
+    
+    /**
+     * 报单价格条件
+     */
+    private char OrderPriceType;
+    
+    /**
+     * 买卖方向
+     */
+    TThostFtdcDirectionType Direction;
+    ///组合开平标志
+    TThostFtdcCombOffsetFlagType CombOffsetFlag;
+    ///组合投机套保标志
+    TThostFtdcCombHedgeFlagType CombHedgeFlag;
+    
+    /**
+     * 价格
+     */
+    private double limitPrice;
+    
+    /**
+     * 数量
+     */
+    private int volumeTotalOriginal;
+    
+    /**
+     * 有效期类型
+     */
+    TThostFtdcTimeConditionType TimeCondition;
+    
+    /**
+     * GTD 日期
+     */
+    private String gTDDate;
+    
+    /**
+     * 成交量类型
+     */
+    TThostFtdcVolumeConditionType VolumeCondition;
+    
+    /**
+     * 最小成交量
+     */
+    private int minVolume;
+    
+    ///触发条件
+    TThostFtdcContingentConditionType ContingentCondition;
+    
+    ///止损价
+    TThostFtdcPriceType StopPrice;
+    
+    ///强平原因
+    TThostFtdcForceCloseReasonType ForceCloseReason;
+    
+    ///自动挂起标志
+    TThostFtdcBoolType IsAutoSuspend;
+    
+    ///业务单元
+    TThostFtdcBusinessUnitTypeBusinessUnit;
+    
+
+}
