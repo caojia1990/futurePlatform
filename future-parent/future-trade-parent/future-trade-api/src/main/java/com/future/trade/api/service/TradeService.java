388f05a0a2b7e0e284f60a1cd467aea48049e932
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-03-01 14:13:14 +0800

    123

--- future-parent/future-trade-parent/future-trade-api/src/main/java/com/future/trade/api/service/TradeService.java
+++ future-parent/future-trade-parent/future-trade-api/src/main/java/com/future/trade/api/service/TradeService.java
@@ -1,5 +1,7 @@
 package com.future.trade.api.service;
 
+import com.future.trade.api.vo.ReqOrderInsertVO;
+
 /**
  * 交易接口
  * @author caojia
@@ -11,6 +13,6 @@ public interface TradeService {
      * 下单接口
      * @return
      */
-    public int reqOrderInsert();
+    public int reqOrderInsert(ReqOrderInsertVO reqOrderInsertVO);
 
 }
---
+++ future-parent/future-trade-parent/future-trade-service/src/main/java/com/future/trade/service/TradeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.future.trade.service;
+
+import com.future.trade.api.service.TradeService;
+import com.future.trade.api.vo.ReqOrderInsertVO;
+
+/**
+ * 交易接口实现
+ * @author caojia
+ *
+ */
+public class TradeServiceImpl implements TradeService{
+
+    @Override
+    public int reqOrderInsert(ReqOrderInsertVO reqOrderInsertVO) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
