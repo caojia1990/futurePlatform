b3c37f6a4a18183cc3f8fb84460b8b732a025d78
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-03-21 16:31:03 +0800

    接口定义

---
+++ future-parent/future-order-parent/future-order-api/src/main/java/com/future/order/api/vo/InvestorPositionDetailVO.java
@@ -0,0 +1,17 @@
+package com.future.order.api.vo;
+
+import java.io.Serializable;
+
+/**
+ * 投资者持仓明细
+ * @author caojia
+ *
+ */
+public class InvestorPositionDetailVO implements Serializable{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -5560766673523616722L;
+
+}
---
+++ future-parent/future-order-parent/future-order-api/src/main/java/com/future/order/api/vo/InvestorPositionVO.java
@@ -0,0 +1,17 @@
+package com.future.order.api.vo;
+
+import java.io.Serializable;
+
+/**
+ * 投资者持仓
+ * @author caojia
+ *
+ */
+public class InvestorPositionVO implements Serializable{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 807079164594930957L;
+
+}
