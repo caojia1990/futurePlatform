18add032c07629f6a05a8844c29f02cbc16efb6e
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-04-04 16:46:51 +0800

    辅助策略

---
+++ future-parent/future-client/src/main/java/com/future/client/strategy/BigVolumeFollow.java
@@ -0,0 +1,32 @@
+package com.future.client.strategy;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import com.future.market.api.vo.DepthMarketData;
+
+/**
+ * 大单对冲跟随
+ * @author caojia
+ */
+public class BigVolumeFollow implements Runnable {
+    
+    private final DepthMarketData marketData;
+    
+    private final StringRedisTemplate redisTemplate;
+    
+    public BigVolumeFollow(DepthMarketData marketData, StringRedisTemplate redisTemplate){
+        this.marketData = marketData;
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+        //成交量
+        int volume = this.marketData.getVolume();
+        //上次成交量
+        //int 
+    }
+
+}
