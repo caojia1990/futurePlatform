ff5b80d36a8369bb29854fa17d589c1431d3056c
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-05-02 16:07:39 +0800

    EMA

---
+++ future-parent/future-client/src/test/java/com/future/client/MarketEMATest.java
@@ -0,0 +1,55 @@
+package com.future.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+@ContextConfiguration(locations = "classpath:applicationContext-test.xml")
+public class MarketEMATest extends AbstractJUnit4SpringContextTests{
+    
+    static Logger logger = Logger.getLogger(MarketEMATest.class);
+    
+    @Resource(name="marketQ")
+    private Queue marketQ;
+    
+    @Resource(name="com.future.market")
+    private TopicExchange topicExchange;
+    
+    @Autowired
+    private RabbitAdmin admin;
+    
+    @Test
+    public void consumer() throws IOException{
+        
+        
+        admin.declareBinding(BindingBuilder.bind(marketQ).to(topicExchange).with("instrument.*"));
+        /*List<String> instrumentIDs = instrumentService.queryInstrumentName();
+        for (String string : instrumentIDs) {
+            admin.declareBinding(BindingBuilder.bind(marketQ).to(topicExchange).with("instrument."+string));
+        }*/
+        
+        
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String str = bufferedReader.readLine();
+            if(str.equals("bye")){
+                break;
+            }
+        }
+        
+    }
+    
+    
+}
---
+++ future-parent/future-client/src/test/java/com/future/client/mq/EMAHandleTest.java
@@ -0,0 +1,78 @@
+package com.future.client.mq;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.future.client.dao.FutureTickDao;
+import com.future.market.api.mq.MessageReceive;
+import com.future.market.api.vo.DepthMarketData;
+
+public class EMAHandleTest implements MessageReceive{
+    
+    static Logger logger = Logger.getLogger(EMAHandleTest.class);
+    
+    @Autowired
+    private FutureTickDao futureTickDao;
+    
+    private Map<String, List<DepthMarketData>> marketDataMap = new HashMap<>();
+    
+    @Override
+    public void handleMessage(DepthMarketData marketData) {
+        
+        String instrumentId = marketData.getInstrumentID();
+        
+        List<DepthMarketData> list = marketDataMap.get(instrumentId);
+        if(list == null){
+            list = new ArrayList<>();
+            marketDataMap.put(instrumentId, list);
+        }
+        list.add(marketData);
+        
+        if(list.size() >= 120){
+            StopWatch stopWatch = StopWatch.createStarted();
+            
+            int r = this.futureTickDao.batchInsert(instrumentId, list);
+            
+            stopWatch.stop();
+            logger.info("插入数据"+r+"条，执行时间：" + stopWatch.getTime());
+            
+            list.clear();
+        }
+        
+        //EMAtoday=α * Pricetoday + ( 1 - α ) * EMAyesterday;
+        
+        
+        
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime time = LocalTime.parse(marketData.getUpdateTime(), formatter);
+        
+        if(list.size() > 0 && time.isAfter(LocalTime.parse("15:00:00")) && time.isBefore(LocalTime.parse("21:00:00"))){
+            //未开盘时间
+            StopWatch stopWatch = StopWatch.createStarted();
+            int r = this.futureTickDao.batchInsert(instrumentId, list);
+            logger.info("日盘收盘插入数据"+r+"条，执行时间：" + stopWatch.getTime());
+            list.clear();
+        }
+        
+        if(list.size() > 0 && time.isAfter(LocalTime.parse("01:00:00")) && time.isBefore(LocalTime.parse("09:00:00"))){
+            //未开盘时间
+            StopWatch stopWatch = StopWatch.createStarted();
+            int r = this.futureTickDao.batchInsert(instrumentId, list);
+            logger.info("夜盘收盘插入数据"+r+"条，执行时间：" + stopWatch.getTime());
+            list.clear();
+        }
+        
+    }
+    
+    
+
+}
