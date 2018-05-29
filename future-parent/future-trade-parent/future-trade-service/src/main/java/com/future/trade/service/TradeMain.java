5e3d5d3c422c7986320840f5da93eb7fb33b7b26
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-05-29 16:13:49 +0800

    添加linux .so支持

--- future-parent/future-trade-parent/future-trade-service/src/main/java/com/future/trade/service/TradeMain.java
+++ future-parent/future-trade-parent/future-trade-service/src/main/java/com/future/trade/service/TradeMain.java
@@ -21,9 +21,17 @@ public class TradeMain {
     static CThostFtdcTraderSpi traderSpi;
     
     static{
-        System.load("D:/git/futurePlatform/future-parent/future-thost-api/src/main/resources/win/thostmduserapi.dll");
-        System.load("D:/git/futurePlatform/future-parent/future-thost-api/src/main/resources/win/thosttraderapi.dll");
-        System.load("D:/git/futurePlatform/future-parent/future-thost-api/src/main/resources/win/thosttraderapi_wrap.dll");
+        
+        String os = System.getProperty("os.name");
+        if(os.toLowerCase().indexOf("win")>=0){
+            System.load("D:/git/futurePlatform/future-parent/future-thost-api/src/main/resources/win/thostmduserapi.dll");
+            System.load("D:/git/futurePlatform/future-parent/future-thost-api/src/main/resources/win/thosttraderapi.dll");
+            System.load("D:/git/futurePlatform/future-parent/future-thost-api/src/main/resources/win/thosttraderapi_wrap.dll");
+        }else {
+            System.loadLibrary("thostmduserapi");
+            System.loadLibrary("thosttraderapi");
+            System.loadLibrary("thosttraderapi_wrap");
+        }
     }
     
     public static void main(String[] args) {
