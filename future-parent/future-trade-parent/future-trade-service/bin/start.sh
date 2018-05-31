42d190638d84f7718f469fcac0ced092bd1d9003
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-05-31 11:18:45 +0800

    启动脚本后台执行

--- future-parent/future-trade-parent/future-trade-service/bin/start.sh
+++ future-parent/future-trade-parent/future-trade-service/bin/start.sh
@@ -4,4 +4,4 @@ cd ..
 DEPLOY_DIR=`pwd`
 LIB_DIR=$DEPLOY_DIR/lib
 LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
-nohup java -Dfile.encoding=UTF-8 -classpath $CONF_DIR:$LIB_JARS$CLASSPATH com.future.trade.service.demo.TradeDemo > stdout.log
+nohup java -Dfile.encoding=UTF-8 -classpath $CONF_DIR:$LIB_JARS$CLASSPATH com.future.trade.service.demo.TradeDemo > stdout.log &
