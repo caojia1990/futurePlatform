5989e63635567512dc98d4dc8fa698c56f7857a0
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-09-17 15:58:02 +0800

    打包

--- future-parent/future-risk/src/main/java/com/future/risk/config/SecurityConfig.java
+++ future-parent/future-risk/src/main/java/com/future/risk/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.future.risk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.future.risk.consumer.UserService;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -22,9 +26,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/resources/**", "/index").permitAll()
             .anyRequest().authenticated()
             .and()
-        .formLogin()
+            .formLogin()
             .and()
-            .httpBasic();
+            .logout();
+        http.csrf().disable();
     }
     
     /*@Autowired
@@ -35,6 +40,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
 }
