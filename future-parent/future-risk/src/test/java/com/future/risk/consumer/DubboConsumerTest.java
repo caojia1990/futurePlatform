bd7faddecd1ddaf630cb2531a4e628f1aaad01b8
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-09-18 15:50:44 +0800

    全局下单板

---
+++ future-parent/future-risk/src/test/java/com/future/risk/consumer/DubboConsumerTest.java
@@ -0,0 +1,26 @@
+package com.future.risk.consumer;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
+import com.future.instrument.api.service.InstrumentService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DubboConsumerTest {
+
+    @Reference(url="")
+    private InstrumentService instrumentService;
+    
+    @Test
+    public void getName(){
+        List<String> list = this.instrumentService.queryInstrumentName();
+        System.out.println(JSON.toJSONString(list));
+    }
+}
