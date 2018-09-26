55b1e102d8795c8dc1698b73f5b7d97f60e936a4
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-09-26 16:42:03 +0800

    平仓减持仓实现

---
+++ future-parent/future-client/src/main/java/com/future/client/service/CloseService.java
@@ -0,0 +1,53 @@
+package com.future.client.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.future.client.dao.TradeDao;
+import com.future.order.api.vo.Direction;
+import com.future.order.api.vo.OnRtnTradeVO;
+
+/**
+ * 平仓实现
+ * @author caojia
+ *
+ */
+@Service("closeService")
+public class CloseService {
+
+    @Autowired
+    private TradeDao tradeDao;
+    
+    /**
+     * 减仓
+     * @param onRtnTrade
+     */
+    public void reducePosition(OnRtnTradeVO onRtnTrade){
+        
+        String direction = onRtnTrade.getDirection() == Direction.BUY ? "1":"0";
+        //查询持仓
+        List<OnRtnTradeVO> list = this.tradeDao.selectByCondition(onRtnTrade.getInvestorId(), onRtnTrade.getAccountNo(), onRtnTrade.getInstrumentId(), direction);
+        if(list != null && list.size() > 0){
+            
+            int volume = onRtnTrade.getVolume();
+            for (OnRtnTradeVO onRtnTradeVO : list) {
+                if(onRtnTradeVO.getVolume() <= volume){
+                    //TODO 删除该笔持仓明细
+                    
+                    volume -= onRtnTradeVO.getVolume();
+                }else {
+                    //TODO 更新该笔持仓明细    剩余持仓为   onRtnTradeVO.getVolume-volume
+                    
+                    volume = 0;
+                }
+                
+                if(volume == 0){
+                    return;
+                }
+            }
+        }
+        
+    }
+}
