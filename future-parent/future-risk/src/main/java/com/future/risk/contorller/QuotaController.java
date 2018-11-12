42c420f8bf785f290e16a0d6787d6d82333523c5
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-11-12 15:59:01 +0800

    指标导入导出

--- future-parent/future-risk/src/main/java/com/future/risk/contorller/QuotaController.java
+++ future-parent/future-risk/src/main/java/com/future/risk/contorller/QuotaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,6 +33,15 @@ public class QuotaController {
     @RequestMapping(value="saveEma", method=RequestMethod.POST)
     public ResponseHead saveEma(@RequestBody EMA ema) throws QuotaException{
         this.emaConsumer.saveEma(ema);
+        return new ResponseHead();
+    }
+    
+    @PostMapping(value="importEma")
+    public ResponseHead importEma(@RequestBody List<EMA> emas) throws QuotaException{
+        
+        for (EMA ema : emas) {
+            this.emaConsumer.saveEma(ema);
+        }
         return new ResponseHead();
     }
     
