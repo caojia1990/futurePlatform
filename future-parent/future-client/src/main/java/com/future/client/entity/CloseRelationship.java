a3bdf52cbc8149d12d126ef69eda362360b62d67
曹佳 <BHQH-CXYWB@BHQH-CXYWB-PC>
2018-11-20 16:55:08 +0800

    优化

---
+++ future-parent/future-client/src/main/java/com/future/client/entity/CloseRelationship.java
@@ -0,0 +1,43 @@
+package com.future.client.entity;
+
+/**
+ * 平仓关系
+ * @author caojia
+ *
+ */
+public class CloseRelationship {
+    
+    //客户端请求ID
+    private String clientRequestId;
+    
+    //开仓成交ID
+    private String openTradeId;
+    
+    //开仓日期
+    private String openDate;
+
+    public String getClientRequestId() {
+        return clientRequestId;
+    }
+
+    public void setClientRequestId(String clientRequestId) {
+        this.clientRequestId = clientRequestId;
+    }
+
+    public String getOpenTradeId() {
+        return openTradeId;
+    }
+
+    public void setOpenTradeId(String openTradeId) {
+        this.openTradeId = openTradeId;
+    }
+
+    public String getOpenDate() {
+        return openDate;
+    }
+
+    public void setOpenDate(String openDate) {
+        this.openDate = openDate;
+    }
+    
+}
